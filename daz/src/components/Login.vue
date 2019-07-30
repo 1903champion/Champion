2b5594160c38a711ba74f13ce848d1e8712d9246
shishuoli <549811751@qq.com>
2019-07-30 20:02:42 +0800

    李杰的

---
+++ daz/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login">
+        <header>
+            <span class="iconfont icon-jiantou8"></span>手机号快速登录
+        </header>
+    </div>
+</template>
+<script>
+
+
+export default {
+    
+}
+</script>
+
+<style>
+@import url("http://at.alicdn.com/t/font_1324710_a1q3i6v5en.css");
+
+</style>
+
