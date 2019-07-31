0d3c5a740ec157e40c220e1388a8c8bcb474109e
yinqingnan <75441755@qq.com>
2019-07-31 11:38:40 +0800

    updata

--- daz/src/main.js
+++ daz/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-// 引入全局的store
-import store from '../store'
+
 Vue.config.productionTip = false
 
 // 引入全局的swiper
@@ -9,7 +8,8 @@ import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
-
+// 引入全局的store
+import store from './store'
 
 
 new Vue({
