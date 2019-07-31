0c7ff5a364b0460bb414fab00cd11b85713850a0
yinqingnan <75441755@qq.com>
2019-07-31 11:00:30 +0800

    update

--- daz/src/components/swiper.vue
+++ daz/src/components/swiper.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <swiper :options="swiperOption" ref="mySwiper" >
-            <!-- slides -->
+            <!--轮播页面 -->
             <swiper-slide>I'm Slide 1</swiper-slide>
             <swiper-slide>I'm Slide 2</swiper-slide>
             <swiper-slide>I'm Slide 3</swiper-slide>
-
-
         </swiper>
+           <!-- 这是按钮指示器 -->
+            <div class="swiper-pagination"></div>
     </div>
 </template>
 <script>
@@ -18,7 +18,9 @@
         swiperOption: {
           // some swiper options/callbacks
           // 所有的参数同 swiper 官方 api 参数
-          
+           pagination: {
+              el: '.swiper-pagination',
+            },
         }
       }
     },
@@ -37,3 +39,15 @@
     },
   }
 </script>
+
+<style scoped>
+.swiper-container {
+      width: 100%;
+      height: 100%;
+    }
+.swiper-slide {
+      text-align: center;
+      font-size: 18px;
+      background: #fff;
+}
+</style>
